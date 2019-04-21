ef238ef6563b88f5ba6760e157fbc16303384599
mac <eng.abdullahshafaal@gmail.com>
2019-04-21 19:11:59 +0300

    editor

--- src/Components/Answer.js
+++ src/Components/Answer.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
+import renderHTML from "react-render-html";
 class Answer extends Component {
   render() {
     const answer = this.props.answer;
-
     return (
       <div>
-        <h4>{answer.a_text}</h4>
+        <h1>{renderHTML(answer.a_text)}</h1>
       </div>
     );
   }
